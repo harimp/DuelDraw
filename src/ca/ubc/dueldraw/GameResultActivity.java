af0eb5b8fdfae4ffa5a430e82691333d45347fb4
tejbirwason <tejbirwason@gmail.com>
2014-11-24 00:24:23 -0800

    added images to results view

--- src/ca/ubc/dueldraw/GameResultActivity.java
+++ src/ca/ubc/dueldraw/GameResultActivity.java
@@ -3,10 +3,14 @@ package ca.ubc.dueldraw;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,6 +33,12 @@ public class GameResultActivity extends Activity {
 		}else{
 			tv.setText("You have lost. Better luck next time!");
 		}
+		
+		ImageView refImage = (ImageView) findViewById(R.id.refImage); 
+		refImage.setImageBitmap(BitmapFactory.decodeFile(Environment.getExternalStorageDirectory().toString() + "/refImage.png"));
+		
+		ImageView userImage = (ImageView) findViewById(R.id.userImage); 
+		refImage.setImageBitmap(BitmapFactory.decodeFile(Environment.getExternalStorageDirectory().toString() + "/userImage.png"));
 	}
 
 	public void backToMainMenu(View view) {
