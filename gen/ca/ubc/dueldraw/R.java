98e0317069c317feadeff4ed8916319e65953a8a
unknown <lomashgupta@gmail.com>
2014-11-26 18:37:31 -0800

    changed the look of buttons and the launcher icon

--- gen/ca/ubc/dueldraw/R.java
+++ gen/ca/ubc/dueldraw/R.java
@@ -18,13 +18,13 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
-        public static final int large_brush=0x7f040004;
-        public static final int medium_brush=0x7f040003;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int large_brush=0x7f050004;
+        public static final int medium_brush=0x7f050003;
         /**  Brush sizes 
          */
-        public static final int small_brush=0x7f040002;
+        public static final int small_brush=0x7f050002;
     }
     public static final class drawable {
         public static final int android=0x7f020000;
@@ -34,7 +34,7 @@ public final class R {
         public static final int dueldrawtext=0x7f020004;
         public static final int eraser=0x7f020005;
         public static final int flappybird=0x7f020006;
-        public static final int ic_launcher=0x7f020007;
+        public static final int ic_action_name=0x7f020007;
         public static final int mushroom=0x7f020008;
         public static final int new_pic=0x7f020009;
         public static final int random=0x7f02000a;
@@ -43,26 +43,28 @@ public final class R {
         public static final int squares=0x7f02000d;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000e;
-        public static final int button1=0x7f090003;
-        public static final int button2=0x7f090004;
-        public static final int button5=0x7f090005;
-        public static final int connect_button=0x7f090001;
-        public static final int imageView1=0x7f09000c;
-        public static final int imageView2=0x7f09000d;
-        public static final int incomingChallengeTextView=0x7f090007;
-        public static final int list=0x7f090000;
-        public static final int opponentID=0x7f09000b;
-        public static final int pixelGridView1=0x7f090006;
-        public static final int refImage=0x7f090009;
-        public static final int result=0x7f090008;
-        public static final int timerTextView=0x7f090002;
-        public static final int userImage=0x7f09000a;
+        public static final int action_settings=0x7f0a0010;
+        public static final int button1=0x7f0a000b;
+        public static final int button2=0x7f0a000d;
+        public static final int clearButton=0x7f0a0005;
+        public static final int connect_button=0x7f0a0001;
+        public static final int drawButton=0x7f0a0003;
+        public static final int eraseButton=0x7f0a0004;
+        public static final int imageView1=0x7f0a000e;
+        public static final int imageView2=0x7f0a000f;
+        public static final int incomingChallengeTextView=0x7f0a0007;
+        public static final int list=0x7f0a0000;
+        public static final int opponentID=0x7f0a000c;
+        public static final int pixelGridView1=0x7f0a0006;
+        public static final int refImage=0x7f0a0009;
+        public static final int result=0x7f0a0008;
+        public static final int timerTextView=0x7f0a0002;
+        public static final int userImage=0x7f0a000a;
     }
     public static final class integer {
-        public static final int large_size=0x7f050002;
-        public static final int medium_size=0x7f050001;
-        public static final int small_size=0x7f050000;
+        public static final int large_size=0x7f060002;
+        public static final int medium_size=0x7f060001;
+        public static final int small_size=0x7f060000;
     }
     public static final class layout {
         public static final int activity_active_players=0x7f030000;
@@ -74,31 +76,31 @@ public final class R {
         public static final int activity_splash=0x7f030006;
     }
     public static final class menu {
-        public static final int active_players=0x7f080000;
-        public static final int connect=0x7f080001;
-        public static final int main=0x7f080002;
-        public static final int menu=0x7f080003;
+        public static final int active_players=0x7f090000;
+        public static final int connect=0x7f090001;
+        public static final int main=0x7f090002;
+        public static final int menu=0x7f090003;
     }
     public static final class string {
-        public static final int accept=0x7f06000e;
-        public static final int action_settings=0x7f060002;
-        public static final int active_players=0x7f06000a;
-        public static final int app_name=0x7f060000;
-        public static final int backToMainMenu=0x7f060012;
-        public static final int blank=0x7f06000d;
-        public static final int challenge=0x7f06000c;
-        public static final int clear=0x7f060007;
-        public static final int connect_button=0x7f060008;
-        public static final int draw=0x7f060004;
-        public static final int erase=0x7f060005;
-        public static final int gameresult=0x7f060011;
-        public static final int hello_world=0x7f060001;
-        public static final int leaderboard=0x7f06000b;
-        public static final int logo=0x7f060009;
-        public static final int placeholder=0x7f060010;
-        public static final int reject=0x7f06000f;
-        public static final int start=0x7f060006;
-        public static final int start_new=0x7f060003;
+        public static final int accept=0x7f07000e;
+        public static final int action_settings=0x7f070002;
+        public static final int active_players=0x7f07000a;
+        public static final int app_name=0x7f070000;
+        public static final int backToMainMenu=0x7f070012;
+        public static final int blank=0x7f07000d;
+        public static final int challenge=0x7f07000c;
+        public static final int clear=0x7f070007;
+        public static final int connect_button=0x7f070008;
+        public static final int draw=0x7f070004;
+        public static final int erase=0x7f070005;
+        public static final int gameresult=0x7f070011;
+        public static final int hello_world=0x7f070001;
+        public static final int leaderboard=0x7f07000b;
+        public static final int logo=0x7f070009;
+        public static final int placeholder=0x7f070010;
+        public static final int reject=0x7f07000f;
+        public static final int start=0x7f070006;
+        public static final int start_new=0x7f070003;
     }
     public static final class style {
         /** 
@@ -122,10 +124,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
+    }
+    public static final class xml {
+        public static final int round_buttons=0x7f040000;
     }
 }
--- src/ca/ubc/dueldraw/ActivePlayersActivity.java
+++ src/ca/ubc/dueldraw/ActivePlayersActivity.java
@@ -12,9 +12,10 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class ActivePlayersActivity extends Activity {
-	ListView listView ;
+	ListView listView;
 	String[] values;
 	SocketApp app;
 	ProgressDialog pd;
@@ -22,75 +23,57 @@ public class ActivePlayersActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE); 
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		setContentView(R.layout.activity_active_players);		 
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(R.layout.activity_active_players);
 		// Get ListView object from xml
-        listView = (ListView) findViewById(R.id.list);
-        
+		listView = (ListView) findViewById(R.id.list);
 
-        app = (SocketApp) getApplicationContext();
-        if(app.playerListReady){
-        	values = app.getPlayerList();
-        }else {
-        	values = new String[] { "Player 1", 
-                    "Player 2",
-                    "Player 3",
-                    "Player 4", 
-                    "Player 5"
-                   };
-        }
-        
-        // Setup adapter for ListViews
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-          android.R.layout.simple_list_item_1, android.R.id.text1, values);
-        listView.setAdapter(adapter); 
-        listView.setOnItemClickListener(new OnItemClickListener()
-        {
-           @Override
-           public void onItemClick(AdapterView<?> adapter, View v, int position,
-                 long arg3) 
-           {
-                String opponentSelected = (String)adapter.getItemAtPosition(position); 
-                app.opponentID = opponentSelected; //set opponentID in application class
-                 
-             	
-             	
-             	app.sendMessage("G"+app.opponentID);	// send challenge
-             	
-             	pd = ProgressDialog.show(ActivePlayersActivity.this, "Challenge Sent",
-             			  "Waiting for Opponent to Accept", true);
-             	
-  	           	new Thread(new Runnable() {
-  	           		  @Override
-  	           		  public void run()
-  	           		  {
-  	           		    // wait for game start ping from DE2
-  	           			while(!app.startGame);
-  	           			app.startGame = false;
-  	           		    runOnUiThread(new Runnable() {
-  	           		      @Override
-  	           		      public void run()
-  	           		      {
-  	           		        pd.dismiss();
-  	           		      }
-  	           		    });
-  	           		  }
-  	           		}).start();
-           }
-        });
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		app = (SocketApp) getApplicationContext();
+		if (app.playerListReady) {
+			values = app.getPlayerList();
+		} else {
+			values = new String[] { "Player 1", "Player 2", "Player 3",
+					"Player 4", "Player 5" };
 		}
-		return super.onOptionsItemSelected(item);
+
+		// Setup adapter for ListViews
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+		listView.setAdapter(adapter);
+		listView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> adapter, View v,
+					int position, long arg3) {
+				String opponentSelected = (String) adapter
+						.getItemAtPosition(position);
+				app.opponentID = opponentSelected; // set opponentID in
+													// application class
+
+				app.sendMessage("G" + app.opponentID); // send challenge
+
+				pd = ProgressDialog.show(ActivePlayersActivity.this,
+						"Challenge Sent", "Waiting for Opponent to Accept",
+						true);
+
+				new Thread(new Runnable() {
+					@Override
+					public void run() {
+						// wait for game start ping from DE2
+						while (!app.startGame)
+							;
+						app.startGame = false;
+						runOnUiThread(new Runnable() {
+							@Override
+							public void run() {
+								pd.dismiss();
+							}
+						});
+					}
+				}).start();
+			}
+		});
 	}
 }
